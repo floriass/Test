a96399792e023032a806f670c9cc8a322d6bfe21
floriass <47177530+floriass@users.noreply.github.com>
2019-01-30 12:26:33 +0100

    Version 2 - med for og if

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,12 @@
 class HelloWorld{
   public static void main(String[] args){
-    System.out.println("Hello World!");
+    for(int i = 0; i<2; i++){
+      if(i = 2){
+        System.out.println("Hello World!");
+      }
+      else{
+        System.out.println(" ");
+      }
+    }
   }
 }
